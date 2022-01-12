7d11d866bfd40d0ad20f2804820a843fb20bfce5
dasaripragna <87476525+dasaripragna@users.noreply.github.com>
2022-01-12 14:20:26 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,23 @@
+# # what is api 
+"API is a shortcut for""Application Programming Interface"
+# why we are using api
+"is a set of rules that are shared by a particular service"
+# what is rest api
+# what is server and client 
+# wht is slug
+
+
+
+# what is http 
+"The Hypertext Transfer Protocol (HTTP) "
+"is designed to enable communications between clients and servers"
+"HTTP works as a request-response protocol between a client and server"
+"Example: A client (browser) sends an HTTP request to the server"
+"then the server returns a response to the client"
+# what is https
+"HTTPS stands for hypertext transfer protocol secure and is the encrypted version of HTTP"
+"It is used for secure communication across the internet or a network"
+"The communication protocol is encrypted using Transport Layer Security (TLS) or formerly Secure Sockets Layer (SSL)"
+
+
+# Difference between http and https
---
+++ requestMeraki.py
@@ -0,0 +1,70 @@
+import requests
+import json
+result=requests.get("https://saral.navgurukul.org/api/courses")
+data=result.json()
+
+with open("availablecourses.json","w") as f:
+    json.dump(data,f,indent=4)
+def option(select,var1,slug,data2):
+    while True: 
+        a=var1
+        print("*********************")
+        options=input("enter your options : up, next, exit back   !")
+        if options=="up":
+            print(a)
+            a-=1
+            req=requests.get("https://saral.navgurukul.org/api/courses/"+str(select)+"/exercise/getbyslug?slug = "+str(slug[a]))
+            x=req.json()
+            print("content",x["content"])    
+    while (True):
+        available_letters = get_available_letters(letters_guessed)
+        print ("Available letters: ") + avai
+        print(a)
+        if options=="next":
+            x+=1
+            req=requests.get("https://saral.navgurukul.org/api/courses/"+str(select)+"/exercise/getbyslug?slug = "+str(slug[a-1]))
+            x1=req.json()
+            print(a)
+        elif options=="back":
+            y=1
+            for dic1 in data2["data"]:
+                print(y,dic1["name"])
+                y+=1
+                print("content",x1["content"])
+                for i in dic1 [" childExercises "]:
+                    print("      ",y,i["name"])
+                    y+=1
+        else:
+            break
+
+
+def course(): 
+    index=0
+    for i in data["availableCourses"]:
+        print(index+1,i["name"],i["id"])
+        index+=1
+    for courses in data["availableCourses"]:
+        course=int(input("enter the your index of course = "))
+        select=data["availableCourses"][courses-1]["id"]
+        var=requests.get("http://saral.navgurukul.org/api/courses/"+str(select)+"/exercise")
+        data2=var.json()
+        count=1
+        slug=[ ]
+        for dic_data2 in data2["data"]:
+            print(count,dic_data2["name"])
+            slug.append(dic_data2["slug"])
+            count=count+1
+            for child in dic_data2["childExercises"]:
+                print(count,child)["name"]
+                slug.append(dic_data2["slug"])
+                count=count+1
+        var2=int(input("select content slug"))
+        var3=requests.get("http://saral.navgurukul.org/api/courses/"+str(select)+"/exercise")
+        data3=var3.json
+        print("content",data3["content"])        
+        option (select,var2,slug,data2)
+course()        
+
+
+
+
